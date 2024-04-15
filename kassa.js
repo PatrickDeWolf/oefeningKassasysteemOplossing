1b097fd7771cf3ae7747555ca33d7508b6be51b2
PatrickDeWolf <64219368+PatrickDeWolf@users.noreply.github.com>
2024-04-15 10:45:14 +0200

    Update kassa.js

--- kassa.js
+++ kassa.js
@@ -5,26 +5,23 @@ var unitPrijs;
 
 switch(expr) {
     case 'appelsien':
-    //document.write('Een appelsien kosten 0,59 euro per stuk.');
     unitPrijs = 0.59;
     result = prompt2 * unitPrijs;
     break;
 
     case 'papayas':
-    //document.write('Mangoes and Papayas kosten 2,49 euro per stuk.');
     unitPrijs = 2.49;
     result = prompt2 * unitPrijs;
     break;
 
     case 'banaan':
-    //document.write('een Banaan is krom en kost 0,49 euro per stuk.');
     unitPrijs = 0.49;
     result = prompt2 * unitPrijs;
     break;
 
     default:
-    //document.write('Sorry, we konden jou product niet weervinden.');
-    result="We konden je product niet weervinden";
+    unitPrijs = 0;
+    result=0;
 }
 
       document.write("<b>Product: </b>" + expr + " (&euro; " +  unitPrijs + ") <b>aantal:</b> " + prompt2 + " <b>Totaal: </b> &euro; " +result);
